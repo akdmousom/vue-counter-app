e66bbea5ab37613091c439b394655da312c38917
AKD <contact.mousom@gmail.com>
2023-12-25 00:18:07 +0600

    Created new MyApp.vue SFC for this project

---
+++ src/MyApp.vue
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<template>
+<h1>My Vue App</h1>
+</template>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import MyApp from './MyApp.vue'
 
-createApp(App).mount('#app')
+createApp(MyApp).mount('#app')
