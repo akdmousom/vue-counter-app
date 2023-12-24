ab4625a885fdf4bef2dc35bc3279074b0bd3cb02
AKD <contact.mousom@gmail.com>
2023-12-25 01:07:27 +0600

    Count Add Button Is Now Working Dynamically

--- src/MyApp.vue
+++ src/MyApp.vue
@@ -1,4 +1,20 @@
-<script>
+<script setup>
+
+import {ref} from 'vue'
+
+const count = ref(0)
+
+const countAdd = ()=>{
+    if (count.value >= 0) {
+
+        count.value++
+        
+    }
+}
+
+
+
+
 </script>
 
 <template >
@@ -7,13 +23,13 @@
         
         <div>
             <h1 class="text-red-500 text-4xl font-bold text-center">My Vue App</h1>
-            <p class="text-red-500 text-3xl my-5 font-bold text-center">Count: </p>
+            <p class="text-red-500 text-3xl my-5 font-bold text-center">Count: {{ count }} </p>
             
             
             <div class="flex justify-center gap-4 my-10">
                 
-                <button class="bg-black hover:first-of-type:text-red-500 hover:first-letter:font-bold  rounded-lg text-white px-2 py-2 text-center">Count +</button>
-                <button class="bg-black hover:last-of-type:text-red-500 hover:first-letter:font-bold   rounded-lg text-white px-2 py-2 text-center">Count -</button>
+                <button @click="countAdd" class="bg-black hover:first-of-type:text-red-500 hover:first-letter:font-bold  rounded-lg text-white px-2 py-2 text-center">Count +</button>
+                <button @click="countMinus" class="bg-black hover:last-of-type:text-red-500 hover:first-letter:font-bold   rounded-lg text-white px-2 py-2 text-center">Count -</button>
 
              </div>
 
