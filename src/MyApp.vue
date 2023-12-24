4bd172cb3a7971ada53e2590469fdf35b589624b
AKD <contact.mousom@gmail.com>
2023-12-25 00:59:15 +0600

    App design finished

--- src/MyApp.vue
+++ src/MyApp.vue
@@ -1,9 +1,29 @@
 <script>
 </script>
 
-<template>
-<h1>My Vue App</h1>
+<template >
+
+    <div class="grid justify-center items-center min-h-screen my-auto">
+        
+        <div>
+            <h1 class="text-red-500 text-4xl font-bold text-center">My Vue App</h1>
+            <p class="text-red-500 text-3xl my-5 font-bold text-center">Count: </p>
+            
+            
+            <div class="flex justify-center gap-4 my-10">
+                
+                <button class="bg-black hover:first-of-type:text-red-500 hover:first-letter:font-bold  rounded-lg text-white px-2 py-2 text-center">Count +</button>
+                <button class="bg-black hover:last-of-type:text-red-500 hover:first-letter:font-bold   rounded-lg text-white px-2 py-2 text-center">Count -</button>
+
+             </div>
+
+
+        </div>
+
+        
+        
+    </div>
+
 </template>
 
-<style>
-</style>
+<style></style>
